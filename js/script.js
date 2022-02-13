dffd031f614f86181f78f1a43f51d9c2db0e60d6
rohitr <rohitrandhir99@gmail.com>
2022-02-13 15:39:55 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var menuBtn = document.getElementById("menuBtn");
+var sideNav = document.getElementById("sideNav");
+var menu = document.getElementById("menu");
+
+sideNav.style.right = "-250px";
+
+menuBtn.onclick = function(){
+    if(sideNav.style.right == "-250px"){
+        sideNav.style.right = "0";
+        menu.src = "img/close.png";
+    }
+    else{
+        sideNav.style.right = "-250px";
+        menu.src = "img/menu.png";
+    }
+}
+
+// Set the scrolling speed for menu bar
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 1000,
+	speedAsDuration: true
+});
